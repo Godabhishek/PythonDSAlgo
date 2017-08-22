ed6e1ddfd28436ac53d74d5eecbdd3457a9cb9d0
Sathish <sathishkc2005@gmail.com>
2017-08-22 15:22:19 +0200

    Update bsearch.py

--- searching/bsearch.py
+++ searching/bsearch.py
@@ -4,7 +4,13 @@ def search (seq,v,l,r):
 #only for arrays its possible, but not for lists
 #time taken is log(r). for eg: if seq is 1000 entries, it needs 10 steps (2^10 = 1024) to complete a search
 #efficency --> O(log n)
-    
+#input of size - log n
+# 10 - 3.3
+# 100 - 6.6
+# 1000 - 10
+# 10^7 - 23
+# 10^10 - 33
+# as you see this one is efficient even for input of size 10^10
 
     if( r -l == 0):
         return False
